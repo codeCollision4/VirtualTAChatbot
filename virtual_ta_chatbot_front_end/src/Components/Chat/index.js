0d6654ad072e9a79bcbd6337f16b751dfe02d884
Zain Ishaq <zainkishaq@gmail.com>
2022-09-27 13:50:55 -0500

    Can send message with TA (no response)

--- virtual_ta_chatbot_front_end/src/Components/Chat/index.js
+++ virtual_ta_chatbot_front_end/src/Components/Chat/index.js
@@ -1,36 +1,38 @@
 import styles from '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput } from '@chatscope/chat-ui-kit-react';
+import React, { useRef } from 'react';
 
-export const Chat = () => {
+
+// import TextInput from '../TextInput'
+
+export const Chat = ({message, setMessage, conversation, setConversation}) => {
+    const inputRef = useRef();
+    const handleSend = message => {
+        setConversation([...conversation, {
+          _id: 3,
+          message,
+          direction: 'outgoing'
+        }]);
+        setMessage("");
+        inputRef.current.focus();
+      };
     return(
         <div style={{ position: 'fixed', bottom: 0, width: '100%', flex:1 }}>
             <MainContainer>
                 <ChatContainer>       
-                <MessageList>
-                    <Message model={{
-                            message: "Hello my friend",
-                            sentTime: "just now",
-                            sender: "Joe"
-                            }} />
-                    <Message model={{
-                            message: "Hi",
-                            sentTime: "just now",
-                            sender: "Jill",
-                            direction: "outgoing",
-                            }} />
-                    <Message model={{
-                            message: "What's Up",
-                            sentTime: "just now",
-                            sender: "Joe"
-                            }} />
-                    <Message model={{
-                            message: "Nothing Much",
-                            sentTime: "just now",
-                            direction: "outgoing",
-                            sender: "Jill"
-                            }} />
-                    </MessageList>
-                <MessageInput placeholder="Type message here" />        
+                <MessageList scrollBehavior="smooth">
+                    {conversation.map((msg, index) => 
+                    <Message key={index} model={msg}/>
+                    )}
+                </MessageList>
+                <MessageInput
+                    attachButton={false} 
+                    onSend={handleSend}
+                    placeholder="Talk with TA here"
+                    value={message}
+                    onChange={(val) => setMessage(val)}
+                    ref={inputRef}
+                />          
                 </ChatContainer>
             </MainContainer>
         </div>
