ef8cdd057e926a3c992f060983a3546b1623fd04
Zain Ishaq <zainkishaq@gmail.com>
2022-10-04 12:26:27 -0500

    Added loading for message

--- virtual_ta_chatbot_front_end/src/Components/Chat/index.js
+++ virtual_ta_chatbot_front_end/src/Components/Chat/index.js
@@ -1,13 +1,17 @@
 import styles from '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
-import { MainContainer, ChatContainer, MessageList, Message, MessageInput } from '@chatscope/chat-ui-kit-react';
-import React, { useRef } from 'react';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, Loader } from '@chatscope/chat-ui-kit-react';
+import {Loading} from 'Components'
+import { useRef, useState, useEffect } from 'react';
 
 
 // import TextInput from '../TextInput'
 
 export const Chat = ({message, setMessage, conversation, setConversation}) => {
     const inputRef = useRef();
+    const [messageLoad, setMessageLoad] = useState("false")
+
     const handleSend = message => {
+        setMessageLoad("true")
         setConversation([...conversation, {
           message,
           direction: 'outgoing'
@@ -15,6 +19,13 @@ export const Chat = ({message, setMessage, conversation, setConversation}) => {
         setMessage("");
         inputRef.current.focus();
       };
+    //Hardcoded load
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          setMessageLoad("false")
+        }, 5000); //change 5000 to whatever time you want
+        return () => clearTimeout(timer);
+      }, [messageLoad]);
     return(
         <div style={{ position: 'fixed', bottom: 0, width: '100%', flex:1 }}>
             <MainContainer>
@@ -23,6 +34,9 @@ export const Chat = ({message, setMessage, conversation, setConversation}) => {
                     {conversation.map((msg, index) => 
                     <Message key={index} model={msg}/>
                     )}
+                <div as="MessageSeparator"> //This is to avoid error of loader not being valid prop
+                    <Loading isvisible={messageLoad} align="left" />
+                </div>
                 </MessageList>
                 <MessageInput
                     attachButton={false} 
---
+++ virtual_ta_chatbot_front_end/src/Components/Loading/index.js
@@ -0,0 +1,11 @@
+import { Loader } from '@chatscope/chat-ui-kit-react';
+
+export const Loading = ({isvisible, align}) => {
+    return (
+        isvisible=='true' ?
+        <div as="Loader" align={align}  style={{paddingTop:'7px', }}>
+            <Loader />
+        </div>
+        : null
+    )
+}
