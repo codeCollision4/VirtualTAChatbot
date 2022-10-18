3c4dfb05b1aa195e5a45dc111fa592945c38ccdf
codeCollision4 <79557266+codeCollision4@users.noreply.github.com>
2022-10-18 12:49:22 -0500

    Undoing body fix attempt

--- virtual_ta_chatbot_back_end/index.js
+++ virtual_ta_chatbot_back_end/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
 
 dotenv.config();
 
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 var jsonParser = bodyParser.json()
-app.use(jsonParser);
+
  
 require("./routes/agentRoutes")(app, jsonParser);
 
