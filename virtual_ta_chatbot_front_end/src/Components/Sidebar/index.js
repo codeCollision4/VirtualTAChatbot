3b469833a2019e8abaa181b70499b227f578dffa
Zain Ishaq <zainkishaq@gmail.com>
2022-11-03 13:31:30 -0500

    Adjusted Color of Sidebar

--- virtual_ta_chatbot_front_end/src/Components/Sidebar/index.js
+++ virtual_ta_chatbot_front_end/src/Components/Sidebar/index.js
@@ -1,22 +1,26 @@
 import {Sidebar, ExpansionPanel  } from '@chatscope/chat-ui-kit-react';
 import {Button, Link } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 import React,{useState} from 'react'
 import SidebarContent from 'Assets/SidebarContent.json'
 
 export const SidebarSection = ({sidebarStyle, setMessage}) => {
+    const theme = useTheme();
+    console.log("t",theme)
     const buttonStyle = {
         width:'100%',
         justifyContent: "flex-start",
         textAlign:'left',
         flex:1,
-        textTransform: 'none'
+        textTransform: 'none',
+        color: theme.palette.text.primary
     }
     return(
         <Sidebar position="left" scrollable={false} style={sidebarStyle}>
             {SidebarContent.QuestionsList.map((content, index) =>
-                    <ExpansionPanel open={false} title={content.title}>
-                        {content.questions.map((question, index) =>
-                            <Button style={buttonStyle} onClick={() => setMessage(question)}>{question}</Button>
+                    <ExpansionPanel key={index} open={false} title={content.title}>
+                        {content.questions.map((question, indexx) =>
+                            <Button  key={indexx} style={buttonStyle} onClick={() => setMessage(question)}>{question}</Button>
                             // <Link style={buttonStyle} color="primary" onClick={() => setMessage(question)}>{question}{"\n"}</Link>
                         )}
                     </ExpansionPanel>
